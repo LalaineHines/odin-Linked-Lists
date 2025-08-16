927db92e3cedb391a98a12ebacbc5035ee9584ec
LalaineHines <lalainehines@gmail.com>
2025-08-16 15:20:43 -0700

    added a main js script file to test functionality of linked list when import to another file

---
+++ main.js
@@ -0,0 +1,31 @@
+import LinkedList from "./linkedList";
+
+const myList = new LinkedList();
+myList.prepend('foo');
+myList.append('bar');
+myList.prepend('ice');
+console.log(myList.toString());
+console.log("size:", myList.size);
+
+console.log(myList.insertAt("cream", 1).toString());
+console.log("size:", myList.size);
+
+console.log(myList.removeAt(2));
+console.log("size:", myList.size);
+
+console.log(myList.insertAt("vanilla", 0).toString());
+console.log("size:", myList.size);
+
+console.log(myList.insertAt("baz", 4).toString());
+console.log("size:", myList.size);
+
+console.log(myList.tail.value);
+
+const animalList = new LinkedList();
+animalList.append("dog");
+animalList.append("cat");
+animalList.append("parrot");
+animalList.append("hamster");
+animalList.append("snake");
+animalList.append("turtle");
+console.log(animalList.toString());
